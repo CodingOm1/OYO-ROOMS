638bbf634ba2f43f46a29e0e8facd6423c381a0e
CodingOm01 <codewithom405@gmail.com>
2024-07-14 10:47:44 +0530

    Booking Style

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -15,7 +15,55 @@ function BookingS() {
 
           </div>
           <div className="bookingInfo">
-            
+            <h2>Classic top 1</h2>
+            <div className="location">
+              <RiMapPin2Fill id="map" /> <p>Ankush Nagar, Old Chrhata Road, Beed -431122</p>
+            </div>
+          </div>
+        </div>
+        <div className="bookBox">
+          <div className="bookingImg">
+            <img
+              src="https://images.pexels.com/photos/1637780/pexels-photo-1637780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Error to load image"
+            />
+
+          </div>
+          <div className="bookingInfo">
+            <h2>Classic top 1</h2>
+            <div className="location">
+              <RiMapPin2Fill id="map" /> <p>Ankush Nagar, Old Chrhata Road, Beed -431122</p>
+            </div>
+          </div>
+        </div>
+        <div className="bookBox">
+          <div className="bookingImg">
+            <img
+              src="https://images.pexels.com/photos/1637802/pexels-photo-1637802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Error to load image"
+            />
+
+          </div>
+          <div className="bookingInfo">
+            <h2>Classic top 1</h2>
+            <div className="location">
+              <RiMapPin2Fill id="map" /> <p>Ankush Nagar, Old Chrhata Road, Beed -431122</p>
+            </div>
+          </div>
+        </div>
+        <div className="bookBox">
+          <div className="bookingImg">
+            <img
+              src="https://images.pexels.com/photos/257499/pexels-photo-257499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Error to load image"
+            />
+
+          </div>
+          <div className="bookingInfo">
+            <h2>Classic top 1</h2>
+            <div className="location">
+              <RiMapPin2Fill id="map" /> <p>Ankush Nagar, Old Chrhata Road, Beed -431122</p>
+            </div>
           </div>
         </div>
       </div>
